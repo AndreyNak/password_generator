a17a1b7025872b1db7fd81c50de0b0b240e4d55a
canina1337 <46906916+canina1337@users.noreply.github.com>
2021-04-07 18:39:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,45 @@
-
-
-const btn = document.getElementById('genPass');
 const input = document.getElementById('input');
 const password = document.getElementById('pass');
+const myvalue = document.getElementById('value');
 
-String.prototype.firstLetterCaps = function() {
-    return this.charAt(0).toUpperCase() + this.slice(1);
+myvalue.innerHTML = input.value;
+
+
+  const func1 = () => {
+    const range = document.getElementById('input');
+    const p = document.getElementById('value');
+    p.innerText = range.value;
+    genPass();
 }
-
-input.addEventListener("keyup", function(event) {
-      if (event.key === 'Enter') {
-        if(input.value) {
-            genPass();
-        }
-      }
-  });
-
 
 
 function genPass() {
+    const combination1 = ['tch','ck','nk','gh','tion','ture','sure', 'igh'];
+    const combination2 = ['wh','qu','wr','kn','wo','wa']
     var vowels = "aeiouy";
     var consonants = "bcdfghjklmnpqrstvwxz"
     let pass = '';
     for (let index = 0; index < input.value; index++) {
-        // const number = Math.floor(Math.random() * array.length);
         pass += consonants.charAt(Math.floor(Math.random() * consonants.length));
         pass += vowels.charAt(Math.floor(Math.random() * vowels.length));
        
     }
-    
-    let number = Math.floor(Math.random() * 9999);
-    password.textContent  = pass+ number;
-    input.value = '';
-}
+    if(pass.length > 4){
+        var elem = Math.floor(Math.random() * combination1.length);
+        elem = combination1[elem];
+        pass = pass.substring(0, pass.length - elem.length);
+        pass = pass + elem;
+        var elem1 = Math.floor(Math.random() * combination2.length);
+        elem1 = combination2[elem1];
+        pass = pass.substring(elem1.length);
+        pass = elem1 + pass;
+    }
+    var number = '';
+    var size = Math.floor(Math.random() * input.value);
+    for (let index = 0; index <  size; index++) {
+        number+='9';    
+    }
+    number = Number(number);
+    password.textContent  = Math.floor(Math.random() * number) + pass + Math.floor(Math.random() * number);
 
-btn.addEventListener('click', function(){
-    genPass();
-})
+}
