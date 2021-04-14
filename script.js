5d094e9a6d1b6c20f89e6a965a3078b38f758878
canina1337 <46906916+canina1337@users.noreply.github.com>
2021-04-14 20:58:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,123 +1,47 @@
+import Costomizator from './costomizator.js';
+
 const input = document.getElementById('input');
-const password = document.getElementById('pass');
-const myvalue = document.getElementById('value');
-const setNumbees = document.getElementById('setNumbees');
+const pass = document.getElementById('pass');
+const value = document.getElementById('value');
+
+const HideNumbersId = document.getElementById('HideNumbers');
+
+const setNumbees  = document.getElementById('setNumbees');
 const countNumbers = document.getElementById('countNumbers');
-const labelCountNums = document.getElementById('labelCountNums');
 const valueCountNums = document.getElementById('valueCountNums');
-const selectSide = document.getElementById('selectSide');
 const numLeft = document.getElementById('numLeft');
 const numRight = document.getElementById('numRight');
 
 
-
-setNumbees.addEventListener('click', function(){
-    if(setNumbees.checked){
-        countNumbers.style.display = "block";
-        labelCountNums.style.display = 'block';
-        valueCountNums.style.display = 'block';
-        selectSide.style.display = 'block';
-
-
-    }else{
-        countNumbers.style.display = "none";
-        labelCountNums.style.display = 'none';
-        valueCountNums.style.display = 'none';
-        selectSide.style.display = 'none';
-    }
-})
-   
-
-myvalue.innerHTML = input.value;
-
-const func2 = () => {
-    const countNumbers = document.getElementById('countNumbers');
-    const valueCountNums = document.getElementById('valueCountNums');
-    valueCountNums.innerText = countNumbers.value;
-    var number = '';
-    var size = countNumbers.value;
-    for (let index = 0; index <  size; index++) {
-        number+='9';    
-    }
-    number = Number(number);
-    return complite(number);
-   
+const options = {
+    numLeft:numLeft,
+    numRight:numRight,
+    countNumbers:countNumbers,
+    valueCountNums:valueCountNums
 }
 
-  const func1 = () => {
-    const range = document.getElementById('input');
-    const p = document.getElementById('value');
-    p.innerText = range.value;
-    genPass();
-}
-
-window.addEventListener('DOMContentLoaded', function(){
-    genPass();
+setNumbees.addEventListener('click', () => {
+    const obj = new Costomizator(options);
+    obj.checkedNums(setNumbees,HideNumbersId);
 })
 
 
-function genPass() {
-    const combination1 = ['tch','ck','nk','gh','tion','ture','sure', 'igh'];
-    const combination2 = ['wh','qu','wr','kn','wo','wa']
-    var vowels = "aeiouy";
-    var consonants = "bcdfghjklmnpqrstvwxz"
-    let pass = '';
-    for (let index = 0; index < input.value; index++) {
-        if (index%2 === 0){
-            pass += consonants.charAt(Math.floor(Math.random() * consonants.length));
-        } else {
-            pass += vowels.charAt(Math.floor(Math.random() * vowels.length));
-        }
-       
-    }
-    if(pass.length > 4){
-        var elem = Math.floor(Math.random() * combination1.length);
-        elem = combination1[elem];
-        pass = pass.substring(0, pass.length - elem.length);
-        pass = pass + elem;
-        var elem1 = Math.floor(Math.random() * combination2.length);
-        elem1 = combination2[elem1];
-        pass = pass.substring(elem1.length);
-        pass = elem1 + pass;
-    }
-    complite(pass);
-}
 
-window.addEventListener('DOMContentLoaded', function(){
-    number = 1;
-    pass = 'abc';
+window.addEventListener('DOMContentLoaded', () => {
+    const obj = new Costomizator(options);
+    pass.innerText = obj.genPass();
+    value.innerText = input.value;
 })
 
 
-function complite(item){
-    if(setNumbees.checked){
-        if(Number.isInteger(item)){
-            number = item;
-            console.log(number);
-            selectNumber(pass, number);
-            countNumbers.style.display = "flex";
-        }else{
-            pass = item;
-            selectNumber(pass, number);
-            countNumbers.style.display = "flex";
-        }
+input.addEventListener('change', () => {
+    const obj = new Costomizator(options);
+    pass.innerText = obj.genPass();
+    value.innerText = input.value;
+})
 
-    } else {
-        pass = item;
-        password.textContent  = pass;
-        countNumbers.style.display = "none";
-    }
-}
-
-function selectNumber(pass, number){
-    if(numLeft.checked && numRight.checked){
-        password.textContent  = Math.floor(Math.random() * number) + pass + Math.floor(Math.random() * number);
-    } else if (numLeft.checked){
-        password.textContent  = Math.floor(Math.random() * number) + pass;
-    } else if (numRight.checked) {
-        password.textContent  = pass + Math.floor(Math.random() * number);
-    } else {
-        password.textContent  = pass;
-    }
-    
-}
+countNumbers.addEventListener('change', () => {
+    const obj = new Costomizator(options);
+    pass.innerText = obj.genPass();
+    value.innerText = input.value;
+})
