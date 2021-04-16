442ff79fcdc1908887ec14f07b96e38a888369ef
canina1337 <46906916+canina1337@users.noreply.github.com>
2021-04-16 23:24:46 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,25 +2,16 @@ import PasswordGeneration from './passwordGenerator.js';
 
 
 const input = document.getElementById('input');
-const lengthLetters = input.value;
-
-// const pass = document.getElementById('pass');
-// const value = document.getElementById('value');
-
+const pass = document.getElementById('pass');
+const value = document.getElementById('value');
 const setNumbees  = document.getElementById('setNumbees');
 const checkSetNumbers = setNumbees.checked ? true : false;
-
 const numLeft = document.getElementById('numLeft');
 const checknumLeft = numLeft.checked ? true : false;
 const numRight = document.getElementById('numRight');
 const checknumRight = numRight.checked ? true : false;
-
 const hideNumbersId = document.getElementById('hideNumbers');
 const countNumbers = document.getElementById('countNumbers');
-// const lengthNumbers = countNumbers.value;
-
-
-
 
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -37,7 +28,6 @@ window.addEventListener('DOMContentLoaded', () => {
     value.innerText = input.value;
     valueCountNums.innerText = countNumbers.value;
 })
-
 
 
 input.addEventListener('input', () => {
@@ -77,6 +67,7 @@ setNumbees.addEventListener('click', () => {
     value.innerText = input.value;
 })
 
+
 setNumbees.addEventListener('click', () => {
     if(setNumbees.checked){
         hideNumbersId.style.display = 'block';
@@ -104,6 +95,7 @@ countNumbers.addEventListener('input', () => {
     valueCountNums.innerText = countNumbers.value;
 })
 
+
 numLeft.addEventListener('click', () => {
     const checkSetNumbers = setNumbees.checked ? true : false;
     const checknumRight = numRight.checked ? true : false;
@@ -123,6 +115,7 @@ numLeft.addEventListener('click', () => {
         pass.innerText = obj.selectNumber();
     }
 })
+
 
 numRight.addEventListener('click', () => {
     const checkSetNumbers = setNumbees.checked ? true : false;
